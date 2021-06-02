7ecfbac3ecd1c400cfe53a0f6e0451ba3d4064ca
Ibraheem <ibraheem.shwiki@gmail.com>
2021-06-02 12:41:50 +0300

    first-commit

---
+++ my_environments/djangoPy3Env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\ibraheem\desktop\python_stack\my_environments\djangopy3env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
