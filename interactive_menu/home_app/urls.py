14c88fda0bc39062a8d1af84d89894678275f4eb
John Doe <johndoe@example.com>
2021-06-03 14:43:32 +0300

    created admin's page amd details page

--- interactive_menu/home_app/urls.py
+++ interactive_menu/home_app/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('details', views.details),
+    path('admin', views.admin),
+
 ]
--- interactive_menu/home_app/views.py
+++ interactive_menu/home_app/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
+from time import gmtime, strftime
+import time
 
 
 def index(request):
     return render(request, 'home.html')
 
 # Create your views here.
+
+
+def details(request):
+    return render(request, 'details.html')
+
+
+def admin (request):
+    context = {
+            "date": strftime("%d %b, %y", gmtime()),
+            "time": time.strftime("%H:%M  %p", time.localtime())}
+    return render (request, 'admin.html', context)
