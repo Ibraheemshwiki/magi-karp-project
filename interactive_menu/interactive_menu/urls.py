0d3c11c03bfa558f1aedfbfca58909ea2e4806d8
Ibraheem <ibraheem.shwiki@gmail.com>
2021-06-03 10:24:01 +0300

    updated static file with thumbnails

--- interactive_menu/interactive_menu/urls.py
+++ interactive_menu/interactive_menu/urls.py
@@ -14,7 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path,include
+from django.contrib import admin
 
 urlpatterns = [
     path('',include('home_app.urls')),
+    path('admin/', admin.site.urls),
 ]
